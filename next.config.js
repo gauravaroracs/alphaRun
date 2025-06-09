2aefb9cf3a732f1b354e39c2615b290853c1e638
gauravaroracs <gauravaroracs@gmail.com>
2025-06-09 15:27:03 +0530

    minor fix

--- next.config.js
+++ next.config.js
@@ -1,25 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Enable static exports
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: '',
-  assetPrefix: '/',
-  trailingSlash: true,
-  i18n: {
-    locales: ['en'],
-    defaultLocale: 'en',
-  },
-  // Disable server-side features
-  experimental: {
-    appDir: false,
-  },
-  // Ensure static export works
-  distDir: 'out',
-  poweredByHeader: false,
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+    reactStrictMode: true,
+    images: {
+      unoptimized: true,
+    },
+    basePath: '',
+    assetPrefix: '/',
+    trailingSlash: true,
+    i18n: {
+      locales: ['en'],
+      defaultLocale: 'en',
+    },
+    experimental: {
+      appDir: false,
+    },
+    poweredByHeader: false,
+  }
